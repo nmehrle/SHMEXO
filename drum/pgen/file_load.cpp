1a58b3879e70653d38bc98ee5f5b8c736d699f4f
Nicholas Mehrle <nmehrle@dhcp-10-29-68-64.dyn.mit.edu>
2022-04-11 14:46:15 -0400

    added new absorbers to problem file

--- drum/pgen/file_load.cpp
+++ drum/pgen/file_load.cpp
@@ -16,8 +16,9 @@
 #include "../utils/utils.hpp"
 #include "../thermodynamics/thermodynamics.hpp"
 #include "../radiation/radiation.hpp"
-#include "../radiation/hydrogen_cia.hpp"
 #include "../radiation/freedman_mean.hpp"
+#include "../radiation/hydrogen_absorber.hpp"
+#include "../radiation/null_absorber.hpp"
 
 // global parameters
 Real user_grav;
@@ -28,13 +29,11 @@ Real G, Mp, Ms, Rp, period, a;
 
 void MeshBlock::InitUserMeshBlockData(ParameterInput *pin)
 {
-  AllocateUserOutputVariables(2);
+  AllocateUserOutputVariables(4);
   SetUserOutputVariableName(0, "temp");
   SetUserOutputVariableName(1, "g1");
-  // SetUserOutputVariableName(2, "ghost_rho");
-  // SetUserOutputVariableName(3, "ghost_press");
-  // SetUserOutputVariableName(4, "ghost_temp");
-  // SetUserOutputVariableName(5, "ghost_g1");
+  SetUserOutputVariableName(2, "radflux");
+  SetUserOutputVariableName(3, "x1flux");
 }
 
 void MeshBlock::UserWorkBeforeOutput(ParameterInput *pin)
@@ -47,43 +46,20 @@ void MeshBlock::UserWorkBeforeOutput(ParameterInput *pin)
         user_out_var(0,k,j,i) = pthermo->Temp(phydro->w.at(k,j,i));
         user_out_var(1,k,j,i) = phydro->hsrc.g1(k,j,i);
 
-      //   user_out_var(2,k,j,i) = 0.0;
-      //   user_out_var(3,k,j,i) = 0.0;
-      //   user_out_var(4,k,j,i) = 0.0;
-      //   user_out_var(5,k,j,i) = 0.0;
-
-      //   // low side ghosts
-      //   if (i - NGHOST <= is) {
-      //     user_out_var(2,k,j,i) = phydro->w(IDN,k,j,i-NGHOST);
-      //     user_out_var(3,k,j,i) = phydro->w(IPR,k,j,i-NGHOST);
-      //     user_out_var(4,k,j,i) = pthermo->Temp(phydro->w.at(k,j,i-NGHOST));
-      //     user_out_var(5,k,j,i) = phydro->hsrc.g1(k,j,i-NGHOST);
-      //   }
-
-      //   if (i + NGHOST >= ie) {
-      //     user_out_var(2,k,j,i) = phydro->w(IDN,k,j,i+NGHOST);
-      //     user_out_var(3,k,j,i) = phydro->w(IPR,k,j,i+NGHOST);
-      //     user_out_var(4,k,j,i) = pthermo->Temp(phydro->w.at(k,j,i+NGHOST));
-      //     user_out_var(5,k,j,i) = phydro->hsrc.g1(k,j,i+NGHOST);
-        // }
+        user_out_var(2,k,j,i) = prad->radflux(k,j,i);
+        user_out_var(3,k,j,i) = phydro->flux[X1DIR](k,j,i);
       }
 }
 
 void RadiationBand::AddAbsorber(std::string name, std::string file, ParameterInput *pin)
-{
-  Real xHe = pin->GetOrAddReal("radiation", "xHe", 0.136);
-  Real xH2 = 1. - xHe;
-
+{ 
   std::stringstream msg;
-
-  if (name == "H2-H2") {
-    pabs->AddAbsorber(XizH2H2CIA(this, 0, xH2))
-        ->LoadCoefficient(file);
-  } else if (name == "H2-He") {
-    pabs->AddAbsorber(XizH2HeCIA(this, 0, xH2, xHe))
-        ->LoadCoefficient(file);
-  } else if (name == "FREEDMAN") {
+  if (name == "FREEDMAN") {
     pabs->AddAbsorber(FreedmanMean(this));
+  } else if (name == "NULL") {
+    pabs->AddAbsorber(NullAbsorber(this));
+  } else if (name == "HYDROGEN") {
+    pabs->AddAbsorber(HydrogenAbsorber(this, pin));
   } else {
     msg << "### FATAL ERROR in RadiationBand::AddAbsorber"
         << std::endl << "unknow absorber: '" << name <<"' ";
@@ -279,14 +255,17 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin)
     }
   }
 
-  // set spectral properties
-  RadiationBand *p = prad->pband;
-  while (p != NULL) {
-    for (int k = kl; k <= ku; ++k)
-      for (int j = jl; j <= ju; ++j)
-        p->SetSpectralProperties(phydro->w, k, j, il, iu);
-    p = p->next;
-  }
+  // // set spectral properties
+  // RadiationBand *p = prad->pband;
+  // while (p != NULL) {
+  //   for (int k = kl; k <= ku; ++k)
+  //     for (int j = jl; j <= ju; ++j)
+  //       p->SetSpectralProperties(phydro->w, k, j, il, iu);
+  //   p = p->next;
+  // }
+  for (int k = ks; k <= ke; ++k)
+    for (int j = js; j <= je; ++j)
+      prad->CalculateFluxes(phydro->w, pmy_mesh->time, k, j, is, ie+1);
 
   peos->PrimitiveToConserved(phydro->w, pfield->bcc, phydro->u, pcoord, is, ie, js, je, ks, ke);
 
