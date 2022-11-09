0016240122d9946cb091ca4a998d1e5d13e21ed8
Nicholas Mehrle <nmehrle@gmail.com>
2022-11-09 18:28:46 -0500

    updated absorber setscalars function checks for valid mass

--- src/radiation/absorber/absorber.hpp
+++ src/radiation/absorber/absorber.hpp
@@ -15,14 +15,15 @@ public:
   RadiationBand *pmy_band;
   PassiveScalars *ps;
 
-  Absorber(RadiationBand *pband): pmy_band(pband), ps(pband->pmy_rad->pmy_block->pscalars) {};
+  Absorber(RadiationBand *pband);
   ~Absorber() {}
 
-  void SetScalar(int n) {scalar_num = n;};
+  void SetScalar(int n);
   virtual Real AbsorptionCoefficient(AthenaArray<Real> const& prim, Real wave, int k, int j, int i);
 
 protected:
   int scalar_num;
+  Real mass;
 };
 
 #endif
