0f6582fbe3921aaa7e7b70f4fae7cfb1cbef281e
Nicholas Mehrle <nmehrle@gmail.com>
2022-11-09 19:10:40 -0500

    asdf

--- src/radiation/absorber/null_absorber.hpp
+++ src/radiation/absorber/null_absorber.hpp
@@ -9,7 +9,7 @@ public:
   NullAbsorber(RadiationBand *pband):
     Absorber(pband){};
   ~NullAbsorber() {};
-  Real AbsorptionCoefficient(AthenaArray<Real> const& prim, Real wave, int k, int j, int i)
+  virtual Real AbsorptionCoefficient(AthenaArray<Real> const& prim, Real wave, int k, int j, int i)
     {return 0;};
 };
 
