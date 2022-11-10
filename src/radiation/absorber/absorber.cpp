07b766fc6db5d8039782f13341fe949d777e82e0
Nicholas Mehrle <nmehrle@gmail.com>
2022-11-09 19:07:32 -0500

    absorber.cpp

--- src/radiation/absorber/absorber.cpp
+++ src/radiation/absorber/absorber.cpp
@@ -1,21 +1,13 @@
-#ifndef ABSORBER_HPP_
-#define ABSORBER_HPP_
-
 // Athena++ headers
 #include "../../athena.hpp"
 #include "absorber.hpp"
-
-class RadiationBand;
-class Radiation;
-class MeshBlock;
-class PassiveScalars;
 
 Absorber::Absorber(RadiationBand *pband) {
   pmy_band = pband;
   ps = pmy_band->pmy_rad->pmy_block->pscalars;
 }
 
-Absorber::SetScalar(int n) {
+void Absorber::SetScalar(int n) {
   scalar_num = n;
   // check n is valid value
   if (n == -1) {
@@ -43,21 +35,3 @@ Absorber::SetScalar(int n) {
     ATHENA_ERROR(msg);
   }
 }
-
-class Absorber {
-public:
-  // data
-  RadiationBand *pmy_band;
-  PassiveScalars *ps;
-
-  Absorber(RadiationBand *pband);
-  ~Absorber() {}
-
-  void SetScalar(int n);
-  virtual Real AbsorptionCoefficient(AthenaArray<Real> const& prim, Real wave, int k, int j, int i);
-
-protected:
-  int scalar_num;
-};
-
-#endif
