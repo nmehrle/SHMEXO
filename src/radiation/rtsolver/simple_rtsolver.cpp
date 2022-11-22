72a60895d3e43d0b7f3d9e5137cbff0d42679a0c
Nicholas Mehrle <nmehrle@gmail.com>
2022-11-21 20:05:09 -0500

    ffff

--- src/radiation/rtsolver/simple_rtsolver.cpp
+++ src/radiation/rtsolver/simple_rtsolver.cpp
@@ -9,6 +9,7 @@
 #include "../../coordinates/coordinates.hpp"
 #include "../../mesh/mesh.hpp"
 #include "../radiation.hpp"
+#include "../absorber/absorber.hpp"
 
 
 SimpleRTSolver::SimpleRTSolver(RadiationBand *pband, ParameterInput *pin):
@@ -24,31 +25,33 @@ void SimpleRTSolver::RadiativeTransfer(MeshBlock *pmb, int n, int k, int j)
 
   // Collect 
   AthenaArray<Real> farea(ie+1);
-  pband->pmy_rad->pmy_block->pcoord->Face1Area(k, j, il, ie+1, farea);
+  pmy_band->pmy_rad->pmy_block->pcoord->Face1Area(k, j, is, ie+1, farea);
 
-  Real Ftop, Fbot, attenuation;
+  int nabs = pmy_band->nabs;
+  Real Ftop = pmy_band->spectral_flux_density(n,k,j,ie+1);
+
+
+  Real Fbot, attenuation, tau_cell;
   Real delta_F, fraction_absorbed, J_i;
-  int nabs = pband->nabs;
-  Ftop = pband->spectral_flux_density(n,k,j,ie+1);
   for (int i = ie; i>=il; --i) {
-    tau_cell = pband->tau_cell(n,k,j,i);
+    tau_cell = pmy_band->tau_cell(n,k,j,i);
     attenuation = exp(-tau_cell);
     Fbot = Ftop * attenuation;
 
     for (int a = 0; a < nabs; ++a) {
-      Absorber *pabs = pband->absorbers(a);
+      Absorber *pabs = pmy_band->absorbers(a);
       //energy absorbed by this absorber
       // Ji = (F1-F0)/dx * (a1 * dx / tau)
 
       delta_F = Ftop - Fbot;
-      fraction_absorbed = pabs->absorptionCoefficient(n,k,j,i) / pband->tau_cell(n,k,j,i);
+      fraction_absorbed = pabs->absorptionCoefficient(n,k,j,i)/tau_cell;
 
       J_i = delta_F * fraction_absorbed;
 
-      pbas->energyAbsorbed(n,k,j,i) = J_i;
+      pabs->energyAbsorbed(n,k,j,i) = J_i;
     }
 
-    pband->spectral_flux_density(n,k,j,i) = Fbot;
+    pmy_band->spectral_flux_density(n,k,j,i) = Fbot;
     Ftop = Fbot;
   }
 }
