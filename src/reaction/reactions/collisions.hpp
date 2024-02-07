37eeabe55f38d92fd59cd693bbd815d6f3c475f9
Nicholas Mehrle <nmehrle@gmail.com>
2024-02-07 14:48:55 -0500

    cleaning up collisions

--- src/reaction/reactions/collisions.hpp
+++ src/reaction/reactions/collisions.hpp
@@ -40,25 +40,3 @@ protected:
   int n_file;
   std::vector<Real> file_T, alpha_data, beta_data;
 };
-
-class He_e_collisions: public Reaction {
-public:
-  He_e_collisions(std::string data_file, std::string name, int singlet_num, int triplet_num, int elec_num);
-  ~He_e_collisions();
-  void react(int k, int j, int i);
-protected:
-  int n_col_type = 4;
-
-  int singlet_scalar_num, triplet_scalar_num, electron_scalar_num;
-  AthenaArray<Real> file_data;
-
-  int n_file;
-
-  Real *file_T;
-  Real **file_q;
-  Real **file_L;
-  enum {x11 = 0, x13 = 1, x31 = 2, x33 = 3};
-
-  Real *temp_q;
-  Real *temp_L;
-};
